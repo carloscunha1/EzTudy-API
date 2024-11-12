a0831d54653705f340e0239c9ead486c6e608490
Carlos <carlosrubim13@gmail.com>
2024-11-12 16:08:38 -0300

    Alteração connect.js para deploy

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'db', // Nome do serviço do docker-compose
-  user: 'root',
-  password: 'root', // teste
-  database: 'eztudy',
-  charset: 'utf8mb4' 
-
+  host: 'junction.proxy.rlwy.net', // Host fornecido pelo Railway
+  user: 'root', // Usuário do banco de dados
+  password: 'IGRLwrDIkSxrAdAVLsNfrOzmrVzqEVLu', // Senha fornecida pelo Railway
+  database: 'railway', // Nome do banco de dados
+  port: 43437, // Porta do banco de dados fornecida pelo Railway (número, não string)
+  charset: 'utf8mb4', // Charset recomendado
 });
- 
+
 module.exports = pool;
